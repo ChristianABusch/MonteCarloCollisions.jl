c3392d101e2688840cc294e84e8955cb71bd6e93
Christian <christian.busch@ruhr-uni-bochum.de>
2024-02-25 13:28:06 +0100

    Added missing export

--- src/MonteCarloCollisions.jl
+++ src/MonteCarloCollisions.jl
@@ -5,6 +5,9 @@ include("misc.jl")
 include("collisions.jl")
 # Write your package code here.
 
-export isotropic_elastic_collision, backscatter_elastic_collision, excitation_collision, ionization_collision, collision_probability, reduced_mass, relative_velocity, energy_in_collision, center_of_mass_velocity, absolute_value, kinetic_energy, to_eV, to_J, Ē, partition_one_by_cross_sections, select_collision
-
+export isotropic_elastic_collision, backscatter_elastic_collision, excitation_collision, 
+       ionization_collision, collision_probability, reduced_mass, relative_velocity, 
+       energy_in_collision, center_of_mass_velocity, absolute_value, kinetic_energy, 
+       relative_energy_and_velocity, to_eV, to_J, Ē, partition_one_by_cross_sections, 
+       select_collision
 end
